1c687915c4eeef36fbac615bc5b22d79bae9a6c8
Jie-Wen Tsai <a840205tw@gmail.com>
2021-11-27 10:37:58 +0800

    Initial commit

---
+++ R/syn_replace.R
@@ -0,0 +1,36 @@
+#'
+#' @title replace synonyms by importing csv file
+#'
+#' @description
+#' @details
+#'
+#' @param df a dataframe includes synonyms and replaced terms
+#' @param data a dataset
+#' @param tag a field-tag, i.g. 'ID' or 'DE'.
+#'
+#' @return a dataframe
+#' @export
+#'
+#' @examples
+#' data(scientometrics, package = "bibliometrixData")
+#' scientometrics$ID <- syn_replace( df = DTF,
+#' data = scientometrics,
+#' tag = 'ID')
+
+syn_replace <- function(df, data, tag){
+  data[[tag]] <- gsub(
+    ';_', replacement = '; ',
+    x= gsub('^_', replacement = ' ',
+            x= gsub('\\s', replacement = '_',
+                    x= gsub('-', replacement = '_-_', data[[tag]]
+                    ) ) ) )
+  for (i in 1:nrow(df)){
+    data[[tag]] <- gsub(
+      x = data[[tag]] ,
+      pattern = paste0( '\\<', df[i, 'pattern'], '\\>' ),
+      replacement = df[i, 'replace'])
+  }
+  data[[tag]] <- gsub('_-_', replacement = '-', x=data[[tag]] )
+  data[[tag]] <- gsub('_', replacement = ' ', x=data[[tag]] )
+  return( data[[tag]] )
+}
